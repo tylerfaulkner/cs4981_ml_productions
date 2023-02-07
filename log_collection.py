d3e75384b50c11ecce5ebf7be464830016c4fa81
tylerfaulkner <tylersfaulkner@gmail.com>
2023-02-06 22:56:11 -0600

    Cleaned Collection Script

--- log_collection.py
+++ log_collection.py
@@ -13,21 +13,21 @@ if __name__ == "__main__":
     while True:
         start = time.time()
         reader = Pygtail('log_file.json')
-        lines = reader.read()
-        print(lines)
+        #print(lines)
         target = boto3.resource('s3',
-            endpoint_url=os.environ['MINIO_URL'],
-            aws_access_key_id=os.environ['MINIO_ID'],
-            aws_secret_access_key=os.environ['MINIO_PASSWORD'],
+            endpoint_url="http://127.0.0.1:9000",
+            aws_access_key_id="minioadmin",
+            aws_secret_access_key="minioadmin",
             aws_session_token=None,
             config=boto3.session.Config(signature_version='s3v4'),
             verify=False
         )
+        lines = reader.read()
         current_time = datetime.datetime.now()
         date = str(current_time.year)[2:4] + str(current_time.month) + str(current_time.day)
         time_str = str(current_time.hour) + str(current_time.minute) + str(current_time.second)
         filename = 'log_file_' + date + '-' + time_str + '.json'
-        bucket = target.Bucket(os.environ['MINIO_LOG_BUCKET'])
+        bucket = target.Bucket("log-files")
         if lines is not None:
             print('Uploading to MinIO')
             bucket.put_object(Key=filename, Body=lines)
